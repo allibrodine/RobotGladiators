8daf6d9098f3825eab63448c9c707a0325d26095
allibrdoine <allibrodine81@gmail.com>
2022-06-05 13:19:13 -0500

    loop enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,8 +14,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    //Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    //repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -66,7 +66,10 @@ var fight = function(enemyName) {
     window.alert("You need to choose a valid optioin. Try again!");
 }
 }
-
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
 }
+for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+  }
+  
